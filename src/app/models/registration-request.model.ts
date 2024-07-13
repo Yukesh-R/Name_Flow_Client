7921a53672e84a886eda751be7b7afea84968ff0
balajis732005 <balajis07032005@gmail.com>
2024-07-13 09:58:47 +0530

    UserServices-Update

---
+++ src/app/models/registration-request.model.ts
@@ -0,0 +1,21 @@
+export interface RegistrationRequestModel {
+
+  firstName : string;
+
+  lastName : string;
+
+  gender : string;
+
+  age : number;
+
+  mobileNumber : string;
+
+  email : string;
+
+  password : string;
+
+  role : string;
+
+  activationCode : string;
+
+}
---
+++ src/app/models/verify-resetpassword.model.ts
@@ -0,0 +1,11 @@
+export interface VerifyResetPasswordModel {
+
+  userId : number;
+
+  email : string;
+
+  activationCode : string;
+
+  newPassword : string;
+
+}
