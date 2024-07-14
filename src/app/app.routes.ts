1dd2396544a218c7037941cffbe8633278b55293
Yukesh <yukeshyukesh54325@gmail.com>
2024-07-14 19:44:53 +0530

    Add HTML for Variable Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
-import {AuthenticationComponent} from "./components/authentication/authentication.component";
-import {RegistrationComponent} from "./components/registration/registration.component";
+import { AuthenticationComponent } from './components/authentication/authentication.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { CreateVariableDialogBoxComponent } from './components/create-variable-dialog-box/create-variable-dialog-box.component';
+import { VariablesComponent } from './components/variables/variables.component';
 
 export const routes: Routes = [
-  {path : '',component : AuthenticationComponent},
-  {path : 'register',component : RegistrationComponent}
+  { path: '', component: AuthenticationComponent },
+  { path: 'register', component: RegistrationComponent },
+  {
+    path: 'all-variables',
+    component: VariablesComponent,
+  },
 ];
--- src/app/components/create-variable-manual-dialog-box/create-variable-manual-dialog-box.component.ts
+++ src/app/components/create-variable-manual-dialog-box/create-variable-manual-dialog-box.component.ts
@@ -2,15 +2,21 @@ import { Component, OnInit, inject, model } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { VariablesComponent } from '../variables/variables.component';
 import { GetVariableRequestModel } from '../../models/get-variable-request-model.Model';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { VariableNameService } from '../../services/variableName/variable-name.service';
 import { CreateVariableManualRequestModel } from '../../models/create-variable-manual-request-model.model';
 import { ResponseModel } from '../../models/response.model';
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-create-variable-manual-dialog-box',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './create-variable-manual-dialog-box.component.html',
   styleUrl: './create-variable-manual-dialog-box.component.css',
 })
@@ -21,6 +27,14 @@ export class CreateVariableManualDialogBoxComponent implements OnInit {
   userId!: number;
   projectId!: number;
 
+  selectedDataType: string = '';
+  isOtherDataTypeSelected: boolean = false;
+  otherDataTypeSelected!: string;
+
+  selectedVarType: string = '';
+  isOtherVarTypeSelected: boolean = false;
+  otherVarTypeSelected!: string;
+
   constructor(private variableNameService: VariableNameService) {}
 
   ngOnInit(): void {
@@ -30,9 +44,7 @@ export class CreateVariableManualDialogBoxComponent implements OnInit {
 
   createVariableManualForm: FormGroup = new FormGroup({
     variableName: new FormControl('', [Validators.required]),
-    dataType: new FormControl('', [Validators.required]),
     description: new FormControl('', [Validators.required]),
-    variableType: new FormControl('', [Validators.required]),
   });
 
   createVariableManual() {
