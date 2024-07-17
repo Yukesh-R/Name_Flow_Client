7ef4fad7a411c1ddba972f5205d42f7fdae5d6ef
Yukesh <yukeshyukesh54325@gmail.com>
2024-07-17 19:52:14 +0530

    Add Form Validation to Login and Register Page

--- src/app/components/authentication/authentication.component.ts
+++ src/app/components/authentication/authentication.component.ts
@@ -1,48 +1,61 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {Store} from "@ngrx/store";
-import {AppState} from "../../store/state/app.state";
-import {AuthenticationRequestModel} from "../../models/authentication-request.model";
-import {authenticationAction} from "../../store/action/authentication.action";
-import {userDetailsSelector} from "../../store/selector/user-details.selector";
-import {UserDetailsModel} from "../../models/user-details.model";
-import {RouterLink} from "@angular/router";
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/state/app.state';
+import { AuthenticationRequestModel } from '../../models/authentication-request.model';
+import { authenticationAction } from '../../store/action/authentication.action';
+import { userDetailsSelector } from '../../store/selector/user-details.selector';
+import { UserDetailsModel } from '../../models/user-details.model';
+import { RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-authentication',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    RouterLink
-  ],
+  imports: [ReactiveFormsModule, RouterLink, NgIf],
   templateUrl: './authentication.component.html',
-  styleUrl: './authentication.component.css'
+  styleUrl: './authentication.component.css',
 })
 export class AuthenticationComponent {
+  constructor(private store: Store<AppState>) {}
 
-  constructor(
-    private store : Store<AppState>
-  ) {
-  }
+  isSubmitted: boolean = false;
 
   loginForm = new FormGroup({
-    email : new FormControl("",[Validators.required, Validators.email]),
-    password : new FormControl("", [Validators.required]),
-  })
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$'),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
 
   onLoginSubmit() {
-    let loginRequest : AuthenticationRequestModel = {
-      email : this.loginForm.value.email!,
-      password : this.loginForm.value.password!,
-    }
+    this.loginForm.markAllAsTouched();
+    this.isSubmitted = true;
+    let loginRequest: AuthenticationRequestModel = {
+      email: this.loginForm.value.email!,
+      password: this.loginForm.value.password!,
+    };
 
-    this.store.dispatch(authenticationAction({
-      authRequest: loginRequest,
-    }))
+    this.store.dispatch(
+      authenticationAction({
+        authRequest: loginRequest,
+      }),
+    );
 
-    this.store.select(userDetailsSelector).subscribe((userDetails : UserDetailsModel) => {
-      console.log(userDetails);
-    })
+    this.store
+      .select(userDetailsSelector)
+      .subscribe((userDetails: UserDetailsModel) => {
+        console.log(userDetails);
+      });
   }
-
 }
