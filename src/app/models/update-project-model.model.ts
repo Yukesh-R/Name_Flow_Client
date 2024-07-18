1cd05acb84eba44aeb15209c4ac94de9bc50585b
Yukesh <yukeshyukesh54325@gmail.com>
2024-07-18 22:20:02 +0530

    Add User Update and User Delete with Project Update with Services

---
+++ src/app/models/update-project-model.model.ts
@@ -0,0 +1,8 @@
+export interface UpdateProjectModel {
+  userId: number;
+  projectId: number;
+  projectName: string;
+  projectDescription: string;
+  techStackDescription: string;
+  projectContributors: string;
+}
