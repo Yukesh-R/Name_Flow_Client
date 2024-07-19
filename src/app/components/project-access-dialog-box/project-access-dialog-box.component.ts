038ad89114be61f0982cf758f9c148d5b483af73
Yukesh <yukeshyukesh54325@gmail.com>
2024-07-19 10:30:07 +0530

    Add Form Validation for create project , project access and update variable

--- src/app/components/project-access-dialog-box/project-access-dialog-box.component.ts
+++ src/app/components/project-access-dialog-box/project-access-dialog-box.component.ts
@@ -1,63 +1,70 @@
-import {Component, inject} from '@angular/core';
-import {MAT_DIALOG_DATA} from "@angular/material/dialog";
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {Store} from "@ngrx/store";
-import {AppState} from "../../store/state/app.state";
-import {userDetailsSelector} from "../../store/selector/user-details.selector";
-import {UserDetailsModel} from "../../models/user-details.model";
-import {CreateRelationshipModel} from "../../models/create-relationship.model";
-import {ProjectService} from "../../services/projectServices/project-services.service";
-import {ResponseModel} from "../../models/response.model";
+import { Component, inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/state/app.state';
+import { userDetailsSelector } from '../../store/selector/user-details.selector';
+import { UserDetailsModel } from '../../models/user-details.model';
+import { CreateRelationshipModel } from '../../models/create-relationship.model';
+import { ProjectService } from '../../services/projectServices/project-services.service';
+import { ResponseModel } from '../../models/response.model';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-project-access-dialog-box',
   standalone: true,
-  imports: [
-    ReactiveFormsModule
-  ],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './project-access-dialog-box.component.html',
-  styleUrl: './project-access-dialog-box.component.css'
+  styleUrl: './project-access-dialog-box.component.css',
 })
 export class ProjectAccessDialogBoxComponent {
-
   constructor(
-    private store : Store<AppState>,
-    private projectService : ProjectService,
-  ) {
-  }
+    private store: Store<AppState>,
+    private projectService: ProjectService,
+  ) {}
 
-  projectIdToGiveAccess : number = inject<number>(MAT_DIALOG_DATA);
+  projectIdToGiveAccess: number = inject<number>(MAT_DIALOG_DATA);
+  isSubmittedAccessForm: boolean = false;
 
   accessForm = new FormGroup({
-    emailToRelation : new FormControl('', [Validators.required,Validators.email]),
-  })
+    emailToRelation: new FormControl('', [
+      Validators.required,
+      Validators.email,
+    ]),
+  });
 
   onAccessFormSubmit() {
-    let userIdFromState : number=-1;
-    this.store.select(userDetailsSelector)
-      .subscribe((userDetails : UserDetailsModel) => {
-        userIdFromState=userDetails.userId;
-      })
+    this.accessForm.markAsTouched();
+    this.isSubmittedAccessForm = true;
+    let userIdFromState: number = -1;
+    this.store
+      .select(userDetailsSelector)
+      .subscribe((userDetails: UserDetailsModel) => {
+        userIdFromState = userDetails.userId;
+      });
 
-    let newAccess : CreateRelationshipModel = {
-      userId : userIdFromState,
-      emailToRelation : this.accessForm.value.emailToRelation!,
-      projectId : this.projectIdToGiveAccess
-    }
+    let newAccess: CreateRelationshipModel = {
+      userId: userIdFromState,
+      emailToRelation: this.accessForm.value.emailToRelation!,
+      projectId: this.projectIdToGiveAccess,
+    };
 
-    this.projectService.createRelationship(newAccess)
-      .subscribe({
-        next: (response : ResponseModel) => {
-          if(response.status){
-            console.log(response);
-          }else{
-            console.log(response.message);
-          }
-        },
-        error: (errorResponse) => {
-          console.log(errorResponse);
+    this.projectService.createRelationship(newAccess).subscribe({
+      next: (response: ResponseModel) => {
+        if (response.status) {
+          console.log(response);
+        } else {
+          console.log(response.message);
         }
-      })
+      },
+      error: (errorResponse) => {
+        console.log(errorResponse);
+      },
+    });
   }
-
 }
